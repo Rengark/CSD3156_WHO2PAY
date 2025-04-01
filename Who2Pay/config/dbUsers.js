a345177e79ea8f49468da08c5aaaaf15fb0d7bf0
Foong Jun Wei <fjunwei178@gmail.com>
2025-04-01 23:14:49 +0800

    updated db passwords
    since they are all the same, we can use the same .env variable, im high asf

--- Who2Pay/config/dbUsers.js
+++ Who2Pay/config/dbUsers.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool(
     {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD_U,
+        password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME_U,
         waitForConnections: true,
         connectionLimit: 10,
