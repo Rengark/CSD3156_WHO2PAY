d2abbb6d317b1066280ab3bd3e02dd91027e1fe8
Jtyz <jjjjtyz@gmail.com>
2025-04-02 16:44:08 +0800

    adding grp owner cfmation

--- Who2Pay/public/js/grp-create.js
+++ Who2Pay/public/js/grp-create.js
@@ -1,4 +1,6 @@
 let members = [];
+//empty owner to be set later
+let owner = "";
 
 function showAddMemberInput() {
     const container = document.getElementById("addMemberContainer");
@@ -7,16 +9,18 @@ function showAddMemberInput() {
         <button id="confirmMember">Confirm</button>
     `;
     document.getElementById('confirmMember').addEventListener('pointerdown', confirmMember);
+    document.getElementById("add").style.visibility = 'hidden';
 }
 
 function confirmMember() {
     const input = document.getElementById("newMemberName");
     const name = input.value.trim();
-    if (name && !members.includes(name)) {
+    if (name && !members.some(item => item.toLowerCase() == name.toLowerCase())) {
         members.push(name);
         updateMemberList();
     }
     document.getElementById("addMemberContainer").innerHTML = ""; // Clear input field
+    document.getElementById("add").style.visibility = 'visible';
 }
 
 function updateMemberList() {
@@ -53,5 +57,40 @@ function togglePassword() {
 }
 
 // Event listener for save button
-document.getElementById('eyeToggle').addEventListener('pointerdown', togglePassword);
+
+
+function switchToGroupOwnerUI() {
+    if(members.length === 0) {
+        return;
+    }
+
+    document.getElementById("createGroupUI").style.display = "none";
+    document.getElementById("groupOwnerUI").style.display = "block";
+    
+    const nameDropdown = document.getElementById("ownerName");
+    nameDropdown.innerHTML = "";
+    members.forEach(member => {
+        const option = document.createElement("option");
+        option.value = member;
+        option.textContent = member;
+        nameDropdown.appendChild(option);
+    });
+}
+
+function goBackToCreateGroup() {
+    document.getElementById("groupOwnerUI").style.display = "none";
+    document.getElementById("createGroupUI").style.display = "block";
+}
+
+document.getElementById('nxtstep').addEventListener('pointerdown', switchToGroupOwnerUI);
+var btcg = document.getElementsByClassName('backtocg');
+for (let i = 0; i < btcg.length; i++) {
+    btcg[i].addEventListener('pointerdown', goBackToCreateGroup);
+}
+
 document.getElementById('add').addEventListener('pointerdown', showAddMemberInput);
+
+var et = document.getElementsByClassName('eyeToggle');
+for (let i = 0; i < et.length; i++) {
+    et[i].addEventListener('pointerdown', togglePassword);
+}
