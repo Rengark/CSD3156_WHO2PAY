5effeaf65c26e6bdaa009f44fa701db933f0c444
Jtyz <jjjjtyz@gmail.com>
2025-04-06 21:32:05 +0800

    added delete button to expanse list page

--- Who2Pay/public/js/expense-list.js
+++ Who2Pay/public/js/expense-list.js
@@ -149,10 +149,10 @@ document.getElementById('navBalances').addEventListener('click', function() {
 });
 
 
-document.getElementById('navSettings').addEventListener('click', function() {
-    console.log('Settings button clicked');
-    // Add your logic to handle settling up
-});
+// document.getElementById('navSettings').addEventListener('click', function() {
+//     console.log('Settings button clicked');
+//     // Add your logic to handle settling up
+// });
 
 
 // Render expenses and navbar when the page loads
@@ -172,9 +172,9 @@ document.addEventListener('DOMContentLoaded', function() {
         window.location.href = '/settle-up.html'; // Redirect to the landing page
     });
     
-    document.getElementById('navSettings').addEventListener('click', function() {
-        window.location.href = '/group-edit.html'; // Redirect to the landing page
-    });
+    // document.getElementById('navSettings').addEventListener('click', function() {
+    //     window.location.href = '/group-edit.html'; // Redirect to the landing page
+    // });
 
     async function checkAuthStatus() {
         try {
@@ -210,3 +210,55 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 });
+
+const ui = {
+    confirm: async (message) => createConfirm(message)
+};
+
+const createConfirm = () => {
+    return new Promise((complete) => {
+
+        
+        const confirmYes = document.getElementById('confirmYes');
+        const confirmNo = document.getElementById('confirmNo');
+        const confirmBox = document.querySelector('.confirm');
+        
+        confirmYes.replaceWith(confirmYes.cloneNode(true));
+        confirmNo.replaceWith(confirmNo.cloneNode(true));
+        
+        document.getElementById('confirmYes').addEventListener('click', () => {
+            confirmBox.style.display = 'none';
+            complete(true);
+        });
+        
+        document.getElementById('confirmNo').addEventListener('click', () => {
+            confirmBox.style.display = 'none';
+            complete(false);
+        });
+        
+        confirmBox.style.display = 'block';
+    });
+};
+
+const save = async () => {
+    const confirm = await ui.confirm();
+    
+    if (confirm) {
+        alert('Group Deleted');
+
+        //go back to landingpage
+        window.location.href = "/landingpage";
+
+        //handle log out here @junwei
+
+        //delete group
+        DeleteGroup();
+    } else {
+    }
+};
+
+
+  
+const deleteButton = document.getElementById('deleteGroup');
+if (deleteButton) {
+    deleteButton.addEventListener('pointerdown', save);
